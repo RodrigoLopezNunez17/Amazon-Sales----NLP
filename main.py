42872c3e9a07e0ad3e61dc3e3b02a7399fede4fb
Rodrigo López Núñez <notroylopnun@gmail.com>
2024-07-25 03:25:56 +0000

    STREAMLIT

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("Amazon Sales (India)")
 st.map(latitude=78.9629, longitude=20.5937, color='green')
 
 
-with pymysql.connect(user='root', passwd='ningunaok12OK@', database='AmazonSales') as connection:
-    fact = pd.read_sql("SELECT * FROM Fact;", connection)
+# with pymysql.connect(user='root', passwd='ningunaok12OK@', database='AmazonSales') as connection:
+#     fact = pd.read_sql("SELECT * FROM Fact;", connection)
 
-st.dataframe(fact)
+# st.dataframe(fact)
